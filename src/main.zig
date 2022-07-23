30c89550f4d5aedb0e527cc4caeb95707b735b54
Yusuke_Saito <ysaito8015@gmail.com>
2022-07-23 12:07:52 +0200

    update

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,41 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    // Note that info level log messages are by default printed only in Debug
-    // and ReleaseSafe build modes.
-    std.log.info("All your codebase are belong to us.", .{});
-}
+    const match = [6]bool{ false, false, true, false, true, false };
+    var count: u8 = 0;
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.os.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+    outer: while (true) {
+        std.debug.print("ジ〜グ ", .{});
+        for (match) |item,i| {
+            var b = rand.boolean();
+            _ = item;
+
+            if (b) {
+                std.debug.print("ジグ ", .{});
+                if (b == match[i]) {
+                    count += 1;
+                }
+            } else {
+                std.debug.print("ザグ ", .{});
+                if (b == match[i]) {
+                    count += 1;
+                }
+            }
+        }
+
+        if (count == 6) {
+            std.debug.print("ひとりきり〜\n", .{});
+            break :outer;
+        } else {
+            count = 0;
+            continue;
+        }
+    }
+    std.debug.print("match で〜す！\n", .{});
 }
